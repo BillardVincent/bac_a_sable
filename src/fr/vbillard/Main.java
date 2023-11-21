2a9253afacda7beb5adff270b30f8b17de039189
BillardVincent <59728481+BillardVincent@users.noreply.github.com>
2023-11-21 10:33:00 +0100

    Update Main.java

--- src/fr/vbillard/Main.java
+++ src/fr/vbillard/Main.java
@@ -3,11 +3,11 @@ package fr.vbillard;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Bonjour et bienvenue !");
+        System.out.printf("Bonjour et bienvenue!");
 
         Utils.compterSept();
 
-        System.out.printf("A bientot !");
+        System.out.printf("A bientot!");
 
     }
 }
