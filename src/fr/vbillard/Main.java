f51b388e801e570375853b8475b0f6a18193d216
vbillard <vincent.billard2@ext.soprasteria.com>
2023-11-21 09:44:01 +0100

    Commit initial

---
+++ src/fr/vbillard/Main.java
@@ -0,0 +1,13 @@
+package fr.vbillard;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Bonjour et bienvenue !");
+
+        Utils.compterCinq();
+
+        System.out.printf("A bientot !");
+
+    }
+}
---
+++ src/fr/vbillard/Utils.java
@@ -0,0 +1,11 @@
+package fr.vbillard;
+
+public class Utils {
+
+    public static  void compterCinq(){
+        System.out.println("Comptons jusqu'à 5 !");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + " !");
+        }
+    }
+}
