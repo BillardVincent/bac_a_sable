344912d7d4cd1025b5f3da782634d01298b3e235
vbillard <vincent.billard2@ext.soprasteria.com>
2023-11-24 14:33:15 +0100

    correction merge

--- src/fr/vbillard/Utils.java
+++ src/fr/vbillard/Utils.java
@@ -5,13 +5,17 @@ public class Utils {
     public static  void compterTrois(){
         System.out.println("Comptons jusqu'à 3 !");
         for (int i = 1; i <= 3; i++) {
+            System.out.println(i + " !");
+
+        }
 
     }
 
-    public static  void compterSept(){
-        System.out.println("Comptons jusqu'à 7 !");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i + " !");
+    public static  void compterSept() {
+            System.out.println("Comptons jusqu'à 7 !");
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(i + " !");
+            }
         }
 
     private Utils(){}
